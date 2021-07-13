cd40116866f6b67de3841e4020565467c836a886
JSFun9888 <53416828+JSFun9888@users.noreply.github.com>
2021-07-13 18:06:26 +1000

    finished the search bar component

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+//import logo from '../../logo.svg';
+//import './App.css';
+import React from 'react';
+import './App.css';
+import BusinessList from '../BusinessList/BusinessList.js';
+import SearchBar from '../SearchBar/SearchBar.js';
+
+function App() {
+  return (
+    <div class="App">
+      <h1>ravenous</h1>
+      <SearchBar />
+      <BusinessList />
+    </div>
+  );
+}
+
+export default App;
